f88187301e076672e3729e1f699498cff21c296c
ZNDLzyl <mwsgyyz@gmail.com>
2020-10-06 11:50:25 +0800

    play the sound by python only

--- playSound.py
+++ playSound.py
@@ -4,6 +4,6 @@ from pydub.playback import play
 # need to pip install PyAudio-0.2.11-cp37-cp37m-win_amd64.whl
 # download from https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyaudio
 
-soundPath = r'D:\Code\PycharmProjects\VoiceSpliter\Test\englishTest.wav'
-song = AudioSegment.from_wav(soundPath)
+soundPath = r'D:\Code\PycharmProjects\VoiceSpliter\Test\afghan-woman-earns-top-marks-on-national-exams.mp3'
+song = AudioSegment.from_file(soundPath, format="mp3")
 play(song)
