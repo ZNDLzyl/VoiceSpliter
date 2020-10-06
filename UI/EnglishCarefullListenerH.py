501449619c737d3c6af3aa19b62044d8b2283400
ZNDLzyl <mwsgyyz@gmail.com>
2020-10-06 17:13:39 +0800

    英语精听功能开发： 1.精听主页面样式实现 2.实现新增音频功能

--- UI/EnglishCarefullListenerH.py
+++ UI/EnglishCarefullListenerH.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtWidgets
 
 from UI.EnglishCarefullListener import Ui_MainWindow
+from tkinter import *
+import tkinter.filedialog
 
 
 class query_window(QtWidgets.QMainWindow):
@@ -8,9 +10,19 @@ class query_window(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.addVoiceButton.clicked.connect(self.addVoice)
+
         # 给button 的 点击动作绑定一个事件处理函数
+        self.ui.addVoiceButton.clicked.connect(self.addVoice)
 
     def addVoice(self):
-        #  此处编写具体的业务逻辑
-        print("")
+        #  弹出弹窗选择文件内容（可以批量）
+        root = Tk()
+        root.withdraw()  # 将Tkinter.Tk()实例隐藏
+        filenames = tkinter.filedialog.askopenfilenames()
+        root.destroy()
+
+        #  拆分选择的文件音频到指定文件夹
+        if len(filenames) is not 0:
+            for filename in filenames:
+                print(filename)
+        #  在音频列表中展示音频项
