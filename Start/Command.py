d5381b94e807ccc56c0bc4e924a8a4ae654d591a
ZNDLzyl <mwsgyyz@gmail.com>
2020-10-09 21:03:47 +0800

    英语精听功能开发： 1.精听主页面样式实现 2.实现新增音频功能 3.音频删除 4.解决子弹窗不能弹出的问题 5.音频播放暂停播放器类的实现 6.解决结束后无法获取结束事件并播放下一首歌的问题

--- Start/Command.py
+++ Start/Command.py
@@ -2,3 +2,9 @@
 
 # python -m pydoc -p 0
 # pip install -i http://mirrors.aliyun.com/pypi/simple/ pygame --trusted-host mirrors.aliyun.com
+
+# pyinstaller -F D:\Code\PycharmProjects\VoiceSpliter\Start\MainStart.py
+
+# pyinstaller.exe -F -w D:\Code\PycharmProjects\VoiceSpliter\Start\MainStart.py
+
+# pyinstaller.exe -F -w D:\Code\PycharmProjects\VoiceSpliter\MainStart.spec
